1f5e0cc04f0c3a0438bd515e20134e38f563008a
Jan <krimdomu@googlemail.com>
2012-09-14 22:08:56 +0200

    added some helper functions for dbal

---
+++ lib/DBIx/ORMapper/Helper.pm
@@ -0,0 +1,49 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+package DBIx::ORMapper::Helper;
+
+use strict;
+use warnings;
+
+require Exporter;
+use base qw(Exporter);
+
+use vars qw(@EXPORT);
+@EXPORT = qw(SELECT INSERT UPDATE DELETE CREATE ALTER);
+
+use DBIx::ORMapper::SQL::Query::SELECT;
+use DBIx::ORMapper::SQL::Query::INSERT;
+use DBIx::ORMapper::SQL::Query::DELETE;
+use DBIx::ORMapper::SQL::Query::UPDATE;
+use DBIx::ORMapper::SQL::Query::ALTER;
+use DBIx::ORMapper::SQL::Query::CREATE;
+
+sub SELECT {
+   return DBIx::ORMapper::SQL::Query::SELECT->new;
+}
+
+sub INSERT {
+   return DBIx::ORMapper::SQL::Query::INSERT->new;
+}
+
+sub DELETE {
+   return DBIx::ORMapper::SQL::Query::DELETE->new;
+}
+
+sub UPDATE {
+   return DBIx::ORMapper::SQL::Query::UPDATE->new;
+}
+
+sub ALTER {
+   return DBIx::ORMapper::SQL::Query::ALTER->new;
+}
+
+sub CREATE {
+   return DBIx::ORMapper::SQL::Query::CREATE->new;
+}
+
+1;
